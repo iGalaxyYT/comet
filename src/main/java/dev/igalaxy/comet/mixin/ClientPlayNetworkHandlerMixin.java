b4f7d97359c469e113ea9ca2a8fecc0f48929092
iGalaxy <hey@igalaxy.dev>
2023-08-25 17:16:31 -0700

    server icon

--- src/main/java/dev/igalaxy/comet/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/dev/igalaxy/comet/mixin/ClientPlayNetworkHandlerMixin.java
@@ -1,17 +1,25 @@
 package dev.igalaxy.comet.mixin;
 
+import dev.igalaxy.comet.modules.discord.CometDiscordModule;
 import dev.igalaxy.comet.modules.hud.CometHudModule;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
 import net.minecraft.network.packet.s2c.play.ScoreboardPlayerUpdateS2CPacket;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
+	@Shadow
+	@Final
+	private MinecraftClient client;
+
 	@Inject(method = "onScoreboardPlayerUpdate", at = @At("HEAD"))
 	private void comet$onScoreboardPlayerUpdate(ScoreboardPlayerUpdateS2CPacket packet, CallbackInfo ci) {
 		ClientPlayerEntity player = MinecraftClient.getInstance().player;
@@ -26,5 +34,18 @@ public class ClientPlayNetworkHandlerMixin {
 				}
 			}
 		}
+	}
+
+	@Inject(method = "onGameJoin", at = @At("TAIL"))
+	private void comet$onGameJoin(GameJoinS2CPacket packet, CallbackInfo ci) {
+		System.out.println(this.client.getCurrentServerEntry());
+		if (this.client.getCurrentServerEntry() != null) {
+			CometDiscordModule.INSTANCE.updateServer(this.client.getCurrentServerEntry());
+		}
+	}
+
+	@Inject(method = "disconnect", at = @At("TAIL"))
+	private void comet$disconnect(CallbackInfo ci) {
+		CometDiscordModule.INSTANCE.clientReady();
 	}
 }
--- src/main/kotlin/dev/igalaxy/comet/modules/discord/CometDiscordModule.kt
+++ src/main/kotlin/dev/igalaxy/comet/modules/discord/CometDiscordModule.kt
@@ -5,6 +5,7 @@ import com.jagrosh.discordipc.entities.RichPresence
 import com.jagrosh.discordipc.entities.pipe.PipeStatus
 import dev.igalaxy.comet.Comet
 import dev.igalaxy.comet.modules.CometModule
+import net.minecraft.client.network.ServerInfo
 import org.quiltmc.loader.api.QuiltLoader
 import org.quiltmc.qsl.lifecycle.api.client.event.ClientLifecycleEvents
 
@@ -46,6 +47,23 @@ object CometDiscordModule : CometModule {
             .setStartTimestamp(startTimestamp)
             .setLargeImage("grass_block", "Minecraft $minecraftVersion")
             .setSmallImage("quilt", "Quilt Loader $quiltVersion")
-        client?.sendRichPresence(builder.build())
+        client.sendRichPresence(builder.build())
+    }
+
+    fun updateServer(serverInfo: ServerInfo) {
+        val minecraftVersion = QuiltLoader.getNormalizedGameVersion()
+        val quiltVersion = QuiltLoader.getModContainer("quilt_loader").get().metadata().version().raw()
+
+        var name = serverInfo.name
+        name = name.replace(Regex("/§[0-9A-FK-OR]/ig"), "")
+
+        val builder = RichPresence.Builder()
+        builder
+            .setStartTimestamp(startTimestamp)
+            .setLargeImage("https://api.mcsrvstat.us/icon/" + serverInfo.address, "Minecraft $minecraftVersion")
+            .setSmallImage("quilt", "Quilt Loader $quiltVersion")
+            .setState("Multiplayer")
+            .setDetails(name)
+        client.sendRichPresence(builder.build())
     }
 }
